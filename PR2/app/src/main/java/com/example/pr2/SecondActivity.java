3d75327c27977b80a5e0c298a66ba0af1d39687c
Rodi1 <s.sortov@gmail.com>
2024-03-12 19:25:00 +0300

    PR2 done, need to redo design

---
+++ PR2/app/src/main/java/com/example/pr2/SecondActivity.java
@@ -0,0 +1,48 @@
+package com.example.pr2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.get("name").toString();
+        String group = arguments.get("group").toString();
+        String age = arguments.get("age").toString();
+        String mark = arguments.get("mark").toString();
+
+        TextView nameView = findViewById(R.id.textView);
+        nameView.setText("Студент: " + name);
+        TextView groupView = findViewById(R.id.textView2);
+        groupView.setText("Группа: " + group);
+        TextView ageView = findViewById(R.id.textView3);
+        ageView.setText("Возраст: " + age);
+        TextView markView = findViewById(R.id.textView4);
+        markView.setText("Желаемая оценка: " + mark);
+
+    }
+
+    public void onPreviousAction(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}

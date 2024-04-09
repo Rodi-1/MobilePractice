ef908dfc7f3d2a46b22b5c6da8d5946f19e3ac18
Rodi1-PC <sortovs@yandex.ru>
2024-04-09 21:44:40 +0300

    PR6 done, test build

--- PR6/app/src/main/java/com/example/pr6/MainActivity.java
+++ PR6/app/src/main/java/com/example/pr6/MainActivity.java
@@ -1,24 +1,100 @@
 package com.example.pr6;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.pr6.task1.p1_fragment_1;
+import com.example.pr6.task1.p1_fragment_2;
+import com.example.pr6.task1.p1_fragment_3;
+import com.example.pr6.task2.MainActivity2;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.nav_pic1){
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView2, p1_fragment_1.class, null).commit();
+                Toast.makeText(MainActivity.this, R.string.p1_dota2, Toast.LENGTH_LONG).show();
+                getSupportActionBar().setTitle("Dota2");
+                return true;
+            }
+            else if (item.getItemId()==R.id.nav_pic2){
+                Toast.makeText(MainActivity.this,R.string.p1_apex_legend, Toast.LENGTH_LONG).show();
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView2, p1_fragment_2.class, null).commit();
+                getSupportActionBar().setTitle("Apex Legends");
+                return true;
+
+            } else if (item.getItemId() == R.id.nav_pic3) {
+                Toast.makeText(MainActivity.this, R.string.p1_league_of_legends, Toast.LENGTH_LONG).show();
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView2, p1_fragment_3.class, null).commit();
+                getSupportActionBar().setTitle("League of Legends");
+                return true;
+
+            } else if (item.getItemId() == R.id.nav_task2) {
+                // Обработка выбора раздела "Задание 2"
+                Toast.makeText(MainActivity.this, R.string.p1_task_2, Toast.LENGTH_LONG).show();
+                startActivity(new Intent(this, MainActivity2.class));
+                return true;
+
+            }
+
+            return false;
         });
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerView2, p1_fragment_1.class, null).commit();
+            navigationView.setCheckedItem(R.id.nav_pic1);
+            getSupportActionBar().setTitle("Dota2");
+        }
+
+        toggle = new ActionBarDrawerToggle(this, drawer, R.string.drawer_open, R.string.drawer_close);
+        if (drawer != null) {
+            Toast.makeText(this, "Drawer != null", Toast.LENGTH_SHORT).show();
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+
     }
+    protected void onResume() {
+        super.onResume();
+    }
+
+
+    // Обработка нажатия на иконку меню в ActionBar для открытия и закрытия Drawer
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Toast.makeText(this, "onOptionsItemSelected", Toast.LENGTH_SHORT).show();
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ PR6/app/src/main/java/com/example/pr6/task1/p1_fragment_1.java
@@ -0,0 +1,25 @@
+package com.example.pr6.task1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pr6.R;
+
+public class p1_fragment_1 extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_p1_1, container, false);
+    }
+}
